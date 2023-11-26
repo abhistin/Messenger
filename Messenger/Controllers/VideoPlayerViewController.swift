f3fa911ccd25f5014cd45081d0dd694c2b261242
Abhishek Bhardwaj <=>
2023-11-26 16:23:38 +0530

    Add View Controllers

---
+++ Messenger/Controllers/VideoPlayerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  VideoPlayerViewController.swift
+//  Messenger
+//
+//  Created by Abhishek Bhardwaj on 26/11/23.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
